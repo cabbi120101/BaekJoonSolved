eeb074a098cad0ee8fb5076aa0bee902014b57e0
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-01-29 11:37:37 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 2552 ms, Memory: 38928 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -20,7 +20,8 @@ primes = prime_list(max_num)
 
 for num in nums:
     cnt = 0
-    
+    # 합이 num이 되는 i, num-i 값 모두 True(소수)일때를 구한다.
+    # 두 수 합이 num이 되는 거니까 num//2의 값까지만 본다.
     for i in range(2, num // 2 + 1):
         if primes[i] and primes[num - i]:
             cnt += 1
