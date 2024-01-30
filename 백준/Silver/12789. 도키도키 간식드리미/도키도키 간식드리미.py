ac94841c0c596bcde9a3e143c37a690f3160f701
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-01-30 10:14:39 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -1,24 +1,19 @@
-import sys
-N = int(input())
-stack2 = []
-cnt = 1
-
-l = list(map(int,sys.stdin.readline().split()))
-while l:
-    if cnt==l[0]:
-        cnt+=1
-        l.pop(0)
+n = int(input())
+standing = list(map(int, input().split()))
+stack = []
+target = 1
+ 
+while standing:
+    if standing[0] == target:
+        target += 1
+        standing.pop(0)
     else:
-        stack2.append(l.pop(0))
-
-    while stack2:
-        if stack2[-1] == cnt:
-            stack2.pop()
-            cnt+=1
+        stack.append(standing.pop(0))
+ 
+    while stack:
+        if stack[-1] == target:
+            stack.pop()
+            target += 1
         else:
             break
-
-if len(stack2)==0:
-    print("Nice")
-else:
-    print("Sad")
+print('Sad' if stack else 'Nice')
