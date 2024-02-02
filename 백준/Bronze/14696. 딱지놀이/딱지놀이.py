b154be46252759cbce4c0d74af71dbe5eeb25886
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-02 16:15:56 +0900

    [Bronze I] Title: 딱지놀이, Time: 184 ms, Memory: 31120 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,16 @@
+def win_chk(num):
+    if num == 0:
+        return 'D'
+    if A.count(num) > B.count(num):
+        return 'A'
+    elif A.count(num) == B.count(num):
+        return win_chk(num-1)
+    else:
+        return 'B'
+
+
+T = int(input())
+for tc in range(1, T+1):
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    print(win_chk(4))
