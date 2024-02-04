641342e288c477669ad50e0f78d077b79d87e6c6
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-04 13:17:06 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,16 +1,11 @@
-N = 9
-arr = [int(input()) for _ in range(9)]
+arr = sorted(int(input()) for _ in range(9))
 cha = sum(arr) - 100
-arr.sort()
 for i in arr:
-    try:
-        if i == cha-i:
-            pass
-        else:
-            arr.remove(cha - i)
-            arr.remove(i)
-            break
-    except:
+    if i == cha-i:
         pass
+    elif cha-i in arr:
+        arr.remove(cha - i)
+        arr.remove(i)
+        break
 for j in arr:
     print(j)
