6e57086e1bc72c6f692d761ebc0d82e389e4ef5c
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-13 10:34:53 +0900

    [Silver V] Title: 수 이어가기, Time: 1056 ms, Memory: 31588 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,17 +1,23 @@
-T = int(input())
-max_num = 0
-ans = []
-def suu(num,num1):
-    if num-num1 < 0:
-        return [num] + [num1]
-    else:
-        return [num] + suu(num1,num-num1)
-for i in range(T,0,-1):
-    tmp = suu(T,i)
-    if len(tmp) > max_num:
-        max_num = len(tmp)
-        ans = tmp
-    elif len(tmp) < max_num:
-        break
-print(max_num)
-print(*ans)
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+max_j = 0
+max_table = []
+for i in range(n,n//2,-1): #절반 이상일동안 값을 하나씩 줄이며 규칙 수행
+    table = [0]*30000
+    table[0] = n
+    table[1] = i
+    table[2] = n-i
+    j = 3
+    while True:
+        table[j] = table[j-2]-table[j-1]
+        if table[j] < 0:
+            break
+        if j>max_j: #최대개수인지 확인
+            max_j = j
+            max_table = table
+        j += 1
+print(max_j+1)
+print(' '.join(map(str,max_table[:max_j+1])))
