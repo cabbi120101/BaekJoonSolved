897dac4b0eddd047042c27453cef9c10db733b83
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-13 16:39:19 +0900

    [Bronze II] Title: 방 배정, Time: 40 ms, Memory: 31120 KB, Score: 2 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -1,16 +1,16 @@
-N, k = map(int, input().split())
-student = [[0] * 7 for _ in range(3)] #성별 / 학년별
+import sys
 
-for _ in range(N):
-    S, Y = map(int, input().split())
-    student[S][Y] += 1
-        
-room = 0
-for i in student:
-    for j in i:
-        if j%k != 0:
-            room += (j // k)+1
-        else:
-            room += (j//k)
+input = sys.stdin.readline
 
-print(room)
+n,k = map(int,input().split())
+
+rooms = [[0]*2 for _ in range(6)] #각 학년별 성별을 담아둔다
+ans = 0
+for _ in range(n): #학생 수만큼 증가
+    s,y = map(int,input().split())
+    rooms[y-1][s] += 1
+
+for i in range(6):
+    for j in range(2):
+        ans += (rooms[i][j]-1)//2 + 1 #각 학년,성별 별로 필요한 방의 수
+print(ans)
