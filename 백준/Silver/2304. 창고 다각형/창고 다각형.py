93a250c1640b8b35373ddb7297f62e62fb4423aa
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-13 09:57:23 +0900

    [Silver II] Title: 창고 다각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,23 +1,55 @@
-N = int(input())
-arr = [0]*1001
-max_H = 0
+import sys
+input  = sys.stdin.readline
+
+n = int(input())
+
+an = []
+max_h = 0
 max_idx = 0
-for _  in range(N):
-    L,H = map(int, input().split())
-    arr[L] = H
-    if max_H < H:
-        max_idx = L
-        max_H = H
+#an에 모든 기둥을 저장하며, 가장 큰 기둥의 위치와 높이를 저장한다.
+for i in range(n):
+    idx,h = map(int,input().split())
+    an.append((idx,h))
+    if h>max_h:
+        max_h = h
+        max_idx = idx
+an.sort(key = lambda x:x[0])
 
-ans = 0
-tmp = 0
-for i in range(max_idx+1):
-    tmp = max(tmp, arr[i])
-    ans += tmp
+points = []
+#max값을 기준으로 왼쪽에서는 좌->우로 보며 커지는 기둥을 points에 저장, 오른쪽에서는 우->좌로 보며 커지는 기둥을 points에 저장한다
+points.append((max_idx,max_h))
+left_max = 0
 
-tmp2 = 0
-for j in range(1000, max_idx, -1):
-    tmp2 = max(tmp2, arr[j])
-    ans += tmp2
+right_max = 0
+i = 0
+while an[i][0]<max_idx:
+    if an[i][1]>=left_max: #max값이랑 같아도 저장한다
+        left_max = an[i][1]
+        points.append(an[i])
+    i+=1
 
-print(ans)
+i = n-1
+while an[i][0]>max_idx:
+    if an[i][1]>=right_max:
+        right_max = an[i][1]
+        points.append(an[i])
+    i-=1
+points.sort(key = lambda x:x[0]) #한번 더 인덱스 기준 정렬
+
+area = 0
+for i in range(0,len(points)): #i+1,i-1 주의
+    if points[i][0]<max_idx: #max보다 이전에 있는 경우
+        area += (points[i+1][0]-points[i][0])*points[i][1]
+    if points[i][0]==max_idx: #max인 경우
+        area += points[i][1]
+    if points[i][0]>max_idx: #max 다음에 있는 경우 
+        area += (points[i][0] - points[i-1][0]) * points[i][1]
+print(area)
+# 7
+# 2 4
+# 11 4
+# 15 6
+# 4 10
+# 5 3
+# 8 10
+# 13 9
