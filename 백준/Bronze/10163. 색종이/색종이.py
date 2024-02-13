d3e5d3c79c51be871cac3f84d9f56540f98d2c34
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-13 10:57:49 +0900

    [Bronze I] Title: 색종이, Time: 280 ms, Memory: 38436 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,9 +1,17 @@
-N = int(input())
-arr = [[0]*1001 for _ in range(1001)]
-for tc in range(1, N+1):
-    i,j, w,h = map(int, input().split())
-    for r in range(j,j+h):
-            arr[r][i:i+w] = [tc]*w
+import sys
 
-for tc in range(1,N+1):
-    print(sum([i.count(tc) for i in arr]))
+input = sys.stdin.readline
+
+n = int(input())
+board = [[0]*1001 for _ in range(1001)]
+for num in range(1,n+1):
+    x,y,width,height = map(int,input().split()) #익순한 x,y좌표배열과 다르지만 정사각형이므로 신경쓰지 않는다
+    for i in range(x,x+width): #각 색종이 위치에 순서값을 대입한다 (이후에 겹치면 덮에씌워짐)
+        for j in range(y,y+height):
+            board[i][j] = num
+
+count_area = [0]*(n+1)
+for r in range(1001): #평면을 돌며 색종이 순서값 카운트
+    for c in range(1001):
+        count_area[board[r][c]] +=1
+print('\n'.join(map(str,count_area[1:])))
