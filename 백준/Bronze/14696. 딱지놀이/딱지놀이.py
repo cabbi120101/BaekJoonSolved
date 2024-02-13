2bc7fc98797d0a5f016a496b1635d5db3dde5d30
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-14 08:55:32 +0900

    [Bronze I] Title: 딱지놀이, Time: 64 ms, Memory: 31120 KB, Score: 32 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -1,16 +1,23 @@
-def win_chk(num):
-    if num == 0:
-        return 'D'
-    if A.count(num) > B.count(num):
-        return 'A'
-    elif A.count(num) == B.count(num):
-        return win_chk(num-1)
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+score_dic = {1:0.0001,2:0.02,3:3,4:301} 
+#각 라운드별로 최대 100개의 그림이 나올 수 있으므로, 
+# 우선순위가 낮은 모양이 100개 나와도 다음 우선순위보다 값이 낮도록 딕셔너리 설정
+for _ in range(n):
+    a,*a_list = list(map(int,input().split()))
+    b,*b_list = list(map(int,input().split()))
+    a_score = 0
+    b_score = 0
+    for item in a_list:
+        a_score+=score_dic[item]
+    for item in b_list:
+        b_score+=score_dic[item]
+    if a_score>b_score:
+        print('A')
+    elif a_score<b_score:
+        print('B')
     else:
-        return 'B'
-
-
-T = int(input())
-for tc in range(1, T+1):
-    A = list(map(int, input().split()))[1:]
-    B = list(map(int, input().split()))[1:]
-    print(win_chk(4))
+        print('D')
