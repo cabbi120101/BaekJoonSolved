ab0c185fc3fe641cce947e1e81eb53ad55e9ff96
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-15 17:18:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze I/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze I/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,11 +1,43 @@
-arr = sorted(int(input()) for _ in range(9))
-cha = sum(arr) - 100
-for i in arr:
-    if i == cha-i:
-        pass
-    elif cha-i in arr:
-        arr.remove(cha - i)
-        arr.remove(i)
-        break
-for j in arr:
-    print(j)
+import sys
+
+input = sys.stdin.readline
+
+def counting_sort(an):
+    count = [0]*100
+    result = [0]*len(an)
+    for item in an:
+        count[item]+=1
+    for i in range(1,100):
+        count[i] += count[i-1]
+    for i in range(len(an)):
+        count[an[i]] -= 1
+        result[count[an[i]]] = an[i]
+    return result
+
+
+
+heights = []
+for _ in range(9):
+    heights.append(int(input()))
+
+def dfs(idx):
+    if len(res):
+        return
+    if len(s)==7:
+        summ = 0
+        for item in s:
+            summ+=heights[item]
+        if summ==100:
+            res.extend(list(s))
+        return
+    for i in range(idx,9):
+        if not i in s:
+            s.append(i)
+            dfs(i)
+            s.pop()
+
+s = []
+res = []
+dfs(0)
+res = counting_sort([heights[x] for x in res])
+print('\n'.join(map(str,res)))
