4258fd8e963e72f4c7a14b0ed368fe949e968e02
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-16 08:36:26 +0900

    [Bronze II] Title: 블랙잭, Time: 300 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze II/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze II/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,15 @@
 N,M = map(int,input().split())
 card = list(map(int,input().split()))
-new_card = []
+new_card = 0
 for i in card:
     for j in card:
         for k in card:
             if i !=j and j!=k and i!=k:
-                new_card.append(i+j+k)
-new_card = sorted(new_card)
-ans = [i for i in new_card if i <= M]
-print(ans[-1])
+                sum_card = i+j+k
+                if sum_card > M:
+                    continue
+                else:
+                    if sum_card > new_card:
+                        new_card = sum_card
+
+print(new_card)
