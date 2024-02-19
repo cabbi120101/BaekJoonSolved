cb4d00e9b23aa7a097696f5d29539fe5fc75eeaf
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-02-20 08:58:16 +0900

    [Silver V] Title: 종이자르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,24 +1,37 @@
-Garo, Sero = map(int, input().split())
-N = int(input())
-Garo_list = []
-Sero_list = []
-def len_max(Gs,li):
+def chk_max_len(arr):
     max_len = 0
-    temp = 0
-    li2 = [0]+sorted(li)+[Gs]
-    for i in range(1,len(li2)):
-        if li2[i]-temp > max_len:
-            max_len = li2[i] - temp
-            temp = li2[i]
-        else:
-            temp = li2[i]
+    for i in range(1, len(arr)):
+        temp_len = arr[i] - arr[i-1]
+        if temp_len > max_len:
+            max_len = temp_len
     return max_len
+def Qsort_list(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    L, P, R = [], [], []
+    for num in arr:
+        if num < pivot:
+            L.append(num)
+        elif num > pivot:
+            R.append(num)
+        else:
+            P.append(num)
+    return Qsort_list(L) + P + Qsort_list(R)
+
+G, S = map(int, input().split())
+N = int(input())
+G_list = []
+S_list = []
+
 for _ in range(N):
-    A, B = map(int, input().split())
-    if A == 1:
-        Garo_list.append(B)
+    gs, num = map(int, input().split())
+    if gs == 0:
+        G_list.append(num)
     else:
-        Sero_list.append(B)
-G = len_max(Garo, Garo_list)
-S = len_max(Sero, Sero_list)
-print(G*S)
+        S_list.append(num)
+
+G_list = [0] + Qsort_list(G_list) + [S]
+S_list = [0] + Qsort_list(S_list) + [G]
+
+print(chk_max_len(G_list)*chk_max_len(S_list))
