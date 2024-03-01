d7e9d27ed0465293ed89266c5c175d97d7c9863f
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-03-01 19:20:28 +0900

    [Gold IV] Title: 연구소, Time: 2820 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,36 @@
+from collections import deque
+import copy
+from itertools import combinations
+
+def bfs(graph):
+    # 바이러스 위치 저장
+    queue = deque([(i,j) for i in range(N) for j in range(M) if graph[i][j] == 2])
+    # 바이러스가 있다면,
+    while queue:
+        r,c = queue.popleft()
+        for i in range(4):
+            dr = r + d[i][0]
+            dc = c + d[i][1]
+            if 0<= dr < N and 0 <= dc < M:
+                # 빈 공간이면
+                if graph[dr][dc] == 0:
+                    # 바이러스 표시
+                    graph[dr][dc] = 2
+                    # queue에 바이러스 좌표 넣기
+                    queue.append((dr,dc))
+    global ans
+    ans = max(ans, sum(i.count(0) for i in graph))
+
+d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+N,M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+empty_space = [(i, j) for i in range(N) for j in range(M) if arr[i][j] == 0]
+ans = 0
+for comb in combinations(empty_space, 3):
+    # 임시 맵 만들기
+    test_map = copy.deepcopy(arr)
+    for y, x in comb:
+        test_map[y][x] = 1
+    bfs(test_map)
+
+print(ans)
