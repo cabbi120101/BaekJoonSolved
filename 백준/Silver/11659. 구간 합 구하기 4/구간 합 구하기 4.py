894eea022d4912212e5371408cd20899a42fe432
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-03-06 11:40:39 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 4984 ms, Memory: 121844 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+arr = list(map(int,input().split()))
+
+for i in range(N-1):
+    arr[i+1] += arr[i]
+arr = [0] + arr
+for _ in range(M):
+    i,j = map(int,input().split())
+    print(arr[j]-arr[i-1])
