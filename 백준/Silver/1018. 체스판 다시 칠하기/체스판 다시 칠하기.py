b60922cb7e22f548550fe9e356a6c86bc9eaf403
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-01 16:03:41 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+arr = [list(input()) for _ in range(N)]
+
+ori_list = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+ori_list_2 = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+
+result = []
+for a in range(N-7):
+    for b in range(M-7):
+        count = 0
+        count_2 = 0
+        for i in range(8):
+            for j in range(8):
+                if arr[a+i][b+j] != ori_list[i][j]:
+                    count += 1
+                if arr[a+i][b+j] != ori_list_2[i][j]:
+                    count_2 += 1
+        if count > count_2:
+            result.append(count_2)
+        else:
+            result.append(count)
+# print(result)
+final_result = min(result)
+
+print(final_result)
