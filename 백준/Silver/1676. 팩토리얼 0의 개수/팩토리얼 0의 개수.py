606c863a46e675a04f053f1742c56e282d36a05d
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-03 09:47:55 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,8 +1,22 @@
-def find_five(n):
-    cnt = 0
-    while n != 0:
-        n //= 5
-        cnt += n
-    return cnt
 N = int(input())
-print(find_five(N))
+
+result = 1
+for i in range(N):
+    result *= (N-i)
+# print(result)
+
+reverse_num = str(result)[::-1]
+# print(reverse_num)
+
+count = 0
+# print(len(reverse_num))
+# print(reverse_num[2]) # 근데 왜 여기는 인식해
+# print(type(reverse_num[2]))
+
+for i in range(len(reverse_num)):
+    if reverse_num[i] == '0':       # 여기서 안됬네
+        count += 1
+    else:
+        break
+
+print(count)
