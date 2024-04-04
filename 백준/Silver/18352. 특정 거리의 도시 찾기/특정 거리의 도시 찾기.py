1127f032778ed433f8d6b018c212dd4e29ca02ef
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-04 17:24:19 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 3192 ms, Memory: 202764 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,31 +1,31 @@
-from collections import deque
-N, M, K, X = map(int, input().split())
-
-graph = [[] for _ in range(N+1)]
-for i in range(M):
-    n1, n2 = map(int, input().split())
-    graph[n1].append(n2)
-
-result = []
-q = deque([(X, 0)])
-visited = [0] * (N+1)
-
-while q:
-    # print(q)
-    sungju, dist = q.popleft()
-    if dist == K and visited[sungju] == 0:
-        result.append(sungju)
-    visited[sungju] = 1
-
-    for i in graph[sungju]:
-        if visited[i] == 1:
+import sys
+import heapq
+def f(graph,start):
+    distances[start] = 0
+    q = []
+    heapq.heappush(q,[distances[start],start])
+    while q:
+        cost,node = heapq.heappop(q)
+        if distances[node] < cost:
             continue
-        else:
-            q.append((i, dist+1))
+        for next_node, next_cost in graph[node].items():
+            dist = cost + next_cost
+            if dist < distances[next_node]:
+                distances[next_node] = dist
+                heapq.heappush(q,[dist,next_node])
 
-result = sorted(result)
-if result:
-    for i in result:
-        print(i)
-else:
-    print('-1')
+
+n,m,k,x = map(int,sys.stdin.readline().split())
+graph = {i:{} for i in range(1,n+1)}
+for i in range(m):
+    a,b = map(int,sys.stdin.readline().split())
+    graph[a].update({b:1})
+distances = {i:float("inf") for i in range(1,n+1)}
+f(graph,x)
+flag= True
+for x,y in distances.items():
+    if y == k:
+        print(x)
+        flag = False
+if flag:
+    print(-1)
