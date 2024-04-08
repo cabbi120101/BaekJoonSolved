5d7eb9c8f03dd0e75a13624a88df7a873fae13c6
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-08 11:35:29 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,19 +1,10 @@
 N = int(input())
-N_ls = sorted(list(map(int, input().split())))
+n_list = set(map(int, input().split()))
 M = int(input())
-M_ls = list(map(int, input().split()))
+m_list = list(map(int, input().split()))
 
-def binary(target, ls, start, end):
-    if start > end:
-        return 0
-    m = (start+end)//2
-    if target == ls[m]:
-        return 1
-    elif target < ls[m]:
-        return binary(target, ls, start, m-1)
+for i in m_list:
+    if i in n_list:
+        print('1')
     else:
-        return binary(target, ls, m+1, end)
-for target in M_ls:
-    start = 0
-    end = N-1
-    print(binary(target, N_ls, start, end))
+        print('0')
