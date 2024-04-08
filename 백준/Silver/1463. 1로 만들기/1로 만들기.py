3024b08aaeadbb0b0a5f1928313561b9a9d68923
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-08 15:59:48 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,10 +1,15 @@
 N = int(input())
-arr = [0] * (N+1)
+memo = [0] * (N+1)
+memo[0] = 0
+memo[1] = 0
+for i in range(2,N+1):
+    if i % 2 == 0 and i % 3 == 0:
+        memo[i] = min(memo[i//2],memo[i//3],memo[i-1]) + 1
+    elif i % 2 == 0:
+        memo[i] = min(memo[i//2],memo[i-1]) + 1
+    elif i % 3 == 0:
+        memo[i] = min(memo[i//3],memo[i-1]) + 1
+    else:
+        memo[i] = memo[i-1] + 1
 
-for i in range(2, N+1):
-    arr[i] = arr[i-1] + 1
-    if i % 3 == 0:
-        arr[i] = arr[i//3] + 1 if arr[i] > arr[i//3]+1 else arr[i]
-    if i % 2 == 0:
-        arr[i] = arr[i//2] + 1 if arr[i] > arr[i//2]+1 else arr[i]
-print(arr[N])
+print(memo[N])
