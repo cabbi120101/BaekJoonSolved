e569deb639c16738f0301da00212496ce6a5bf18
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-15 10:46:04 +0900

    [Silver III] Title: 소수 구하기, Time: 5432 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,11 @@
-M, N = map(int, input().split())
-def prime(x):
-    if x<2:
-        return False
-    for i in range(2,int(x**0.5)+1):
-        if x % i == 0:
-            return False
-    return True
-for i in range(M,N+1):
-    if prime(i):
+N, M = map(int, input().split())
+
+for i in range(N, M+1):
+
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            break
+    else:
         print(i)
