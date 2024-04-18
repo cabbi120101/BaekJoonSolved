97798567ea4d8bccbf887a4c9433c5be08b4932d
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-04-18 14:36:37 +0900

    [Silver I] Title: 쿼드트리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,24 +1,17 @@
 def quadtree(row, col, n):
     save = arr[row][col]
-    flag = True
     for i in range(n):
         for j in range(n):
             if arr[row+i][col+j] != save:
-                print('(', end='')
-                quadtree(row, col, n//2)
-                quadtree(row, col+n//2, n//2)
-                quadtree(row+n//2, col, n//2)
-                quadtree(row+n//2, col+n//2, n//2)
-                print(')', end='')
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(save, end='')
+                # print('(', end='')
+                return '(' + quadtree(row, col, n//2) + quadtree(row, col+n//2, n//2) + quadtree(row+n//2, col, n//2) + quadtree(row+n//2, col+n//2, n//2)  + ')'
+                # print(')', end='')
+        # print(save, end='')
+    return str(save)
 
 
 N = int(input())
 arr = [list(map(int, input())) for _ in range(N)]
 
-quadtree(0, 0, N)
+a = quadtree(0, 0, N)
+print(a)
