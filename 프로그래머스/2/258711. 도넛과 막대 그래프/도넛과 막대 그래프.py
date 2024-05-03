2982ce48adb339a26b31ebc4440fa22b18ce341a
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-05-03 19:50:10 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -0,0 +1,24 @@
+def solution(edges):
+    graph = dict()
+    for start, end in edges:
+        if start in graph:
+            graph[start][0] += 1
+        else: # out, in
+            graph[start] = [1,0]
+        if end in graph:
+            graph[end][1] += 1
+        else:
+            graph[end] = [0,1]
+    
+    ans = [0,0,0,0]
+    for node, count in graph.items():
+        if count[0] >= 2 and count[1]==0:
+            ans[0] = node
+        elif count[0] == 0 and count[1] >= 1:
+            ans[2] += 1
+        elif count[0] >= 2 and count[1]>=2:
+            ans[3] += 1
+    ans[1] = graph[ans[0]][0] - ans[2] - ans[3]
+    return ans
+    
+    
