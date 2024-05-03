2e0d0fac89c996579177e0ac7089fa80cc53ca3f
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-05-03 20:45:13 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -12,10 +12,13 @@ def solution(edges):
     
     ans = [0,0,0,0]
     for node, count in graph.items():
+        # 시작 점
         if count[0] >= 2 and count[1]==0:
             ans[0] = node
+        # 막대 그래프
         elif count[0] == 0 and count[1] >= 1:
             ans[2] += 1
+        # 8자 그래프
         elif count[0] >= 2 and count[1]>=2:
             ans[3] += 1
     ans[1] = graph[ans[0]][0] - ans[2] - ans[3]
