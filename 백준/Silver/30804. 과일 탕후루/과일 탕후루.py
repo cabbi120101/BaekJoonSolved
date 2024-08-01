7537d918cdfe834e37fff2a5f2125f883dcc819e
cabbi120101 <85271909+cabbi120101@users.noreply.github.com>
2024-08-01 15:00:38 +0900

    [Silver II] Title: 과일 탕후루, Time: 144 ms, Memory: 34600 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -1,25 +1,25 @@
+def two_point(n, tang):
+    left = 0
+    max_len = 0
+    count = [0] * 10
+    one_fruit = 0
+
+    for right in range(n):
+        if count[tang[right]] == 0:
+            one_fruit += 1
+        count[tang[right]] += 1
+
+        while one_fruit > 2:
+            count[tang[left]] -= 1
+            if count[tang[left]] == 0:
+                one_fruit -= 1
+            left += 1
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
 N = int(input())
 tang = list(map(int, input().split()))
-
-
-def two_point(n, fruits):
-    left = 0
-    fruits_count = {}
-    max_len = 0
-
-    for right in range(N):
-        if fruits[right] in fruits_count:
-            fruits_count[fruits[right]] += 1
-        else:
-            fruits_count[fruits[right]] = 1
-        
-        while len(fruits_count) > 2:
-            fruits_count[fruits[left]] -= 1
-            if fruits_count[fruits[left]] == 0:
-                del fruits_count[fruits[left]]
-            left += 1
-        
-        max_len = max(max_len, right-left +1)
-    return max_len
 
 print(two_point(N, tang))
